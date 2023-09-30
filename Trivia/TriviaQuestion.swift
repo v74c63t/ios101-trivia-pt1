1c8e9db40c6e275b07befafe155167209a0d9a1e
Vanessa Tang <vanesst5@uci.edu>
2023-09-30 16:44:13 -0700

    Added outlets for all labels and created trivia question struct

---
+++ Trivia/TriviaQuestion.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaQuestion.swift
+//  Trivia
+//
+//  Created by Vanessa Tang on 9/30/23.
+//
+
+import Foundation
+
+struct TriviaQuestion{
+    let question: String
+    let topic: String
+    let answer1: String
+    let answer2: String
+    let answer3: String
+    let answer4: String
+    let num: Int
+}
--- Trivia/TriviaViewController.swift
+++ Trivia/TriviaViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class TriviaViewController: UIViewController {
 
+    @IBOutlet weak var questionNum: UILabel!
+    @IBOutlet weak var questionTopic: UILabel!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var answer1: UIButton!
+    @IBOutlet weak var answer2: UIButton!
+    @IBOutlet weak var answer3: UIButton!
+    @IBOutlet weak var answer4: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
