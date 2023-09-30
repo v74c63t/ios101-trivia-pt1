632a03f98289dd9f32e37e819f95f14ed1d286f2
Vanessa Tang <vanesst5@uci.edu>
2023-09-30 16:35:34 -0700

    Added basic ui

---
+++ Trivia/TriviaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TriviaViewController.swift
+//  Trivia
+//
+//  Created by Vanessa Tang on 9/30/23.
+//
+
+import UIKit
+
+class TriviaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
